ae58e3796b0168183b8023641106cf9f16d5fc35
Rebecca Turner <rbt@sent.as>
2019-09-13 19:39:13 -0400

    Bug fixes, improve ra_lsp_server
    - update bootstrap to only do install stuff as root
    - avoid filters-as-tests in fd install
    - update hexd build process (maybe apply this to pixd as well...?)
    - Fix ra_lsp_server build process
    - Make rust-analyzer build not say it's building hexd 🙄:

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,22 @@
 #! /bin/bash
 set -e
 
-apt update
-apt install -y python-pip
-pip install ansible
-sudo -u $(hostname) ansible-playbook -K site.yml
+if [[ "$(id -u)" -eq 0 ]]
+then
+    echo "First-time installation running as root."
+    set -v
+    apt update
+    apt install -y python-pip
+    pip install ansible
+    sudo -u $(hostname) ansible-playbook -K site.yml -l "$(hostname)" $@
+    set +v
+else
+    if ! command -v ansible-playbook > /dev/null
+    then
+        echo "ansible-playbook not found; did you mean to run as root and install prereqs?"
+        exit 1
+    fi
+    set -v
+    ansible-playbook site.yml -l "$(hostname)" $@
+    set +v
+fi
